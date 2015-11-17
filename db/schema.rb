d09d5b44908027b903946ea7d4cc91a5c3debc1d
Hunter Nix <hunter.nix@avisonyoung.com>
2015-11-16 19:16:12 -0600

    add edit and destroy projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151114185605) do
+ActiveRecord::Schema.define(version: 20151117005732) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -42,6 +42,9 @@ ActiveRecord::Schema.define(version: 20151114185605) do
     t.string   "link"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "slug"
   end
+
+  add_index "projects", ["slug"], name: "index_projects_on_slug", unique: true
 
 end
