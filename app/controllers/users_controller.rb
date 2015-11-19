5f9a8b9f426fc29523c6658b67e77703569e595f
Hunter Nix <hunter.nix@avisonyoung.com>
2015-11-18 20:59:25 -0600

    setup login redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
 
   private
   def find_user
-    @user = User.find(params[:id])
+    @user = User.find(1)
   end 
   def user_params
     params.require(:user).permit(:email, :about)
