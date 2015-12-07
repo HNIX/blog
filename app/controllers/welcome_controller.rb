0a7b4291a13a7f7ff58d3fd5403b3efc683581fa
Hunter Nix <hunter.nix@avisonyoung.com>
2015-12-06 22:18:55 -0600

    list all objects on welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,8 @@ class WelcomeController < ApplicationController
   	@user = User.find(1)
   	@posts = Post.all.limit(3).order("created_at desc")
   	@projects = Project.all.limit(3).order("created_at desc")
+  	@videos = Video.all.limit(3).order("created_at desc")
+  	@combined_sorted = (@videos + @projects + @posts).sort{|a,b| a.created_at <=> b.created_at }
   end
 
 end
